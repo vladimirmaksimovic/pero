bc8a745022a471185d04e91ecb4e6ec399b245ea
VM <vmaksimovic.cr34@gmail.com>
2022-05-03 22:35:39 +0200

    styling aside form section

--- view/aside.php
+++ view/aside.php
@@ -9,11 +9,15 @@ $cena = isset($cena) ? $cena : "";
 
 <!-- aside -->
 <aside>
+  <fieldset>
+    <legend>Unos podataka</legend>
     <form action="../controller/automobili.php" method="get">
-        <label for="marka">Marka</label>
-        <input type="text" id="marka" name="marka" value="<?= $marka; ?>" />
-        <label for="cena">Cena</label>
-        <input type="number" id="cena" name="cena" value="<?= $cena; ?>" />
-        <input type="submit" name="action" value="Unos" />
+      <label for="marka">Marka</label>
+      <input type="text" id="marka" name="marka" value="<?= $marka; ?>" />
+      <label for="cena">Cena</label>
+      <input type="number" id="cena" name="cena" value="<?= $cena; ?>" />
+      <hr>
+      <input type="submit" name="action" value="Unos" />
     </form>
+  </fieldset>
 </aside>
